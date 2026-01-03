1342d58c7cb2c53648905f02c30dd7204dea4e4c
Fransisca-somto <fransiscasomto@gmail.com>
2026-01-03 03:51:53 +0100

    REchecked

--- main.py
+++ main.py
@@ -12,5 +12,15 @@ def home():
     emails = email_extract.getemails(data["URLS"])
     return emails
 
+@app.route('/emails', methods=['POST'])
+def extract_emails():
+    data = request.get_json()
+    emails = email_extract.getemails(data["URLS"])
+    emails_response = emails["Responses"]
+    only_emails = []
+    for email_list in emails_response.values():
+        only_emails.extend(email_list)
+    return jsonify({"emails": only_emails})
+
 if __name__ == '__main__':
     app.run(debug=True)
